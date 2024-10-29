9e4aca74886be1f050d8b28784f7bb5a20d29d21
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-10-29 12:17:17 +0100

    ball following cursor and changing coour when left mouse button is pressed

--- main.cpp
+++ main.cpp
@@ -3,13 +3,37 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    const int screenWeidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWeidth, screenHeight, "raylib [core] example - basic window");
+
+    Vector2 ballPosition = {-100.00f,-100.00f};
+    Color ballColor = RED;
+    Color colors[8] = {RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, BROWN, DARKGRAY};
+    int currentColor = 0;
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        ballPosition = GetMousePosition();
+        
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            ballColor = colors[currentColor%8];
+            currentColor++;
+        }
+
+        //Draw screen
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            ClearBackground(RAYWHITE);
+            DrawCircleV(ballPosition, 50, ballColor);
+            DrawText("move ball with mouse and click button to change colour", 190, 200, 20, LIGHTGRAY);
+            
+
         EndDrawing();
     }
 
